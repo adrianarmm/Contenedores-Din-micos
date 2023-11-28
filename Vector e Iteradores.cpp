d18e30a4d737b257766f4993482f3c42d8be8383
adrianarmm <areyemor@myuax.com>
2023-11-28 11:12:56 +0100

    2

--- Vector e Iteradores.cpp
+++ Vector e Iteradores.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 #include <vector>
+
+int main()
+{
+    std::vector<int> Vector;
+    Vector.push_back(10);
+    Vector.push_back(20);
+    Vector.push_back(30);
+    Vector.push_back(40);
