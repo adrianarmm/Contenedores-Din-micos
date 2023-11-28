a7a9bd8bc0833edc1db8368a8fd3356e75155fe8
adrianarmm <areyemor@myuax.com>
2023-11-28 11:21:54 +0100

    7

---  Contenedores Estándar y Secuencias.cpp
+++  Contenedores Estándar y Secuencias.cpp
@@ -5,3 +5,7 @@
 int main()
 {
     std::list<double> decimalList;
+    decimalList.push_back(20.8);
+    decimalList.push_back(17.8);
+    decimalList.push_back(3.6);
+    decimalList.push_back(5.4);
