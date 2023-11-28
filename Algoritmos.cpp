db84a37d6630aa83748261bb07ffbfedb0680c00
adrianarmm <areyemor@myuax.com>
2023-11-28 11:28:02 +0100

    14

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -4,4 +4,10 @@
 
 int main()
 {
-    std::vector<int> Vector = {9, 2, 5, 1, 7, 4, 8, 3, 6};
+    std::vector<int> miVector = {9, 2, 5, 1, 7, 4, 8, 3, 6};
+    std::sort(miVector.begin(), miVector.end());
+
+    std::cout << "Vector ordenado: ";
+    for (int num : miVector) {
+        std::cout << num << " ";
+    }
