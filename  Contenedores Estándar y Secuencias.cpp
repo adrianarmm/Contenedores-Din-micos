d02b94a50a2c8c91cf510b7b32a2ffc3c50e1eeb
adrianarmm <areyemor@myuax.com>
2023-11-28 11:22:34 +0100

    9

---  Contenedores Estándar y Secuencias.cpp
+++  Contenedores Estándar y Secuencias.cpp
@@ -11,3 +11,9 @@ int main()
     decimalList.push_back(5.4);
 
     std::stack<double, std::list<double>> decimalStack(decimalList);
+
+    std::cout << "Elementos en la pila:" << std::endl;
+    while (!decimalStack.empty()) {
+        std::cout << "Top: " << decimalStack.top() << std::endl;
+        decimalStack.pop();
+    }
