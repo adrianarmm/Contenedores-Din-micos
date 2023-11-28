e9a0606edd30817d476c1254a19265e7bac6e0c4
adrianarmm <areyemor@myuax.com>
2023-11-28 11:17:15 +0100

    3

--- Vector e Iteradores.cpp
+++ Vector e Iteradores.cpp
@@ -8,3 +8,8 @@ int main()
     Vector.push_back(20);
     Vector.push_back(30);
     Vector.push_back(40);
+
+    std::cout << "Elementos del vector:" << std::endl;
+    for (std::vector<int>::iterator it = Vector.begin(); it != Vector.end(); ++it) {
+        std::cout << *it << " ";
+    }
