951f4285a07d2ca28359c5f2534673ddfc59c907
adrianarmm <areyemor@myuax.com>
2023-11-29 18:25:57 +0100

    47

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,20 @@
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+#include <string>
+
+int main() {
+    int number = 42;
+    std::string numberAsString = boost::lexical_cast<std::string>(number);
+    std::cout << "Integer as String: " << numberAsString << std::endl;
+
+    std::string numStr = "3.14";
+    try {
+        double parsedDouble = boost::lexical_cast<double>(numStr);
+        std::cout << "String as Double: " << parsedDouble << std::endl;
+    } catch(const boost::bad_lexical_cast& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
 
