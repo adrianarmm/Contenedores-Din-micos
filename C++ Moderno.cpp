2bfb7a7daac17dfcee953f43c84f078c5d07c4cb
adrianarmm <areyemor@myuax.com>
2023-11-29 17:40:33 +0100

    35

--- C++ Moderno.cpp
+++ C++ Moderno.cpp
@@ -17,3 +17,7 @@ int main()
     std::cout << "Recorriendo el contenedor con for_each: ";
     std::for_each(numbers.begin(), numbers.end(), [](int n) { std::cout << n << " "; });
     std::cout << std::endl;
+
+    std::cout << "Uso de punteros inteligentes:" << std::endl;
+    std::shared_ptr<int> smartPtr(new int(10)); // Ejemplo con shared_ptr
+    std::cout << "Valor almacenado en el smart pointer: " << *smartPtr << std::endl;
