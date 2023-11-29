dbc0d6ff2a0a6c6b5547754cda90609528880a62
adrianarmm <areyemor@myuax.com>
2023-11-29 17:36:36 +0100

    23

--- Cálculo numérico.cpp
+++ Cálculo numérico.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
-
     std::valarray<double> array1 = {1.0, 2.0, 3.0, 4.0};
     std::valarray<double> array2 = {2.0, 1.5, 2.5, 3.5};
+
+    std::valarray<double> sum = array1 + array2;
+    std::valarray<double> difference = array1 - array2;
