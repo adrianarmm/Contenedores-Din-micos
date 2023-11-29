e354e31eee6ba31e3c366b3421743ba00e142382
adrianarmm <areyemor@myuax.com>
2023-11-29 17:26:58 +0100

    20

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+int main() {
+    std::vector<int> myVector = {9, 2, 5, 1, 7, 4, 8, 3, 6};
+    std::sort(myVector.begin(), myVector.end());
+
+    std::cout << "Vector ordenado: ";
+    for (int num : myVector) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    auto maxElement = std::max_element(myVector.begin(), myVector.end());
+
