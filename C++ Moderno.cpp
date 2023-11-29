0113490ff5d5992172a01f7b96d0aa0488b6e5c5
adrianarmm <areyemor@myuax.com>
2023-11-29 17:40:20 +0100

    34

--- C++ Moderno.cpp
+++ C++ Moderno.cpp
@@ -11,3 +11,9 @@ int main()
     auto result = square(number);
 
     std::cout << "El cuadrado de " << number << " es " << result << std::endl;
+
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    std::cout << "Recorriendo el contenedor con for_each: ";
+    std::for_each(numbers.begin(), numbers.end(), [](int n) { std::cout << n << " "; });
+    std::cout << std::endl;
